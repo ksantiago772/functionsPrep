e0105a04fa6deae701097c9ff0d89b56798652b8
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-10-25 17:37:31 +0000

    abstraction

---
+++ main.py
@@ -0,0 +1,15 @@
+apples = int(input("how many apples"))
+oranges = int(input("how many oranges"))
+
+from add import add_fruit
+from divide import divide_fruit
+from multiply import multiply_fruit
+#function call for adding apples and oranges
+fruit = add_fruit (apples, oranges)
+print(fruit)
+#function for dividing apples and oranges
+divide = divide_fruit(apples,oranges)
+print(divide)
+#function for multiplying apples and oranges
+multiply = multiply_fruit(apples,oranges)
+print(multiply)
---
+++ multiply.py
@@ -0,0 +1,2 @@
+def multipy_fruit(apples,oranges):
+  return apples + oranges
